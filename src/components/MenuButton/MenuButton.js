791189de75d068501f4994a21af67054d9208827
Ayush Acharya <ayushjash1323@gmail.com>
2021-03-02 00:21:03 +0530

    Added menu to MenuButton component

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { IconButton, makeStyles } from "@material-ui/core";
+import { IconButton, makeStyles, Menu, MenuItem } from "@material-ui/core";
 import themeColors from "../../constants/themeColors";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -28,19 +29,59 @@ const useStyles = makeStyles(theme => ({
         margin: "0.4rem 0 0.3rem 0.5rem",
         width: "1.5rem",
     },
-
+    menu: {
+        "& .MuiPaper-root": {
+            background: themeColors.red,
+            borderRadius: 0,
+            boxShadow: "none",
+        }
+    },
+    menuItem: {
+        color: "white",
+        fontFamily: "'Poppins', sans-serif",
+        fontSize: "0.8rem",
+        fontWeight: 600,
+        margin: "0.2rem 1rem",
+        textTransform: "uppercase",
+    },
 }));
 
 function MenuButton() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const history = useHistory();
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const openProfile = () => {
+        history.push("/profile");
+    };
     return (
-        <IconButton className={classes.root}>
-            <div className={classes.logo}>
-                <div className={classes.line1} />
-                <div className={classes.line2} />
-                <div className={classes.line3} />
-            </div>
-        </IconButton>
+        <React.Fragment>
+            <IconButton className={classes.root} onClick={handleClick}>
+                <div className={classes.logo}>
+                    <div className={classes.line1} />
+                    <div className={classes.line2} />
+                    <div className={classes.line3} />
+                </div>
+            </IconButton>
+            <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                className={classes.menu}
+            >
+                <MenuItem className={classes.menuItem} onClick={openProfile}>Profile</MenuItem>
+                <MenuItem className={classes.menuItem} onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </React.Fragment>
     );
 }
 
