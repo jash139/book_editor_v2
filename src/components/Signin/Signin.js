20eda61466de8af2b544ddefb6f95eeabb74f4a6
Ayush Acharya <ayushjash1323@gmail.com>
2021-03-27 18:55:12 +0530

    Centered the signin contents, added styling

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { Button, TextField, Typography } from '@material-ui/core';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { Button, TextField, Typography } from "@material-ui/core";
+
+import Heading from "../Heading/Heading";
+import EntryNavbar from "../EntryNavbar/EntryNavbar";
 
 import { useAuth } from "../../contexts/AuthContext";
-
 import themeColors from "../../constants/themeColors";
-import Heading from '../Heading/Heading';
-import EntryNavbar from '../EntryNavbar/EntryNavbar';
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        minHeight: "85vh",
+        [theme.breakpoints.down("sm")]: {
+            minHeight: "90vh",
+        },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
     card: {
         minHeight: 600,
         maxWidth: 400,
@@ -128,64 +137,66 @@ function Signin() {
     }
 
     return (
-        <div className={classes.root}>
+        <React.Fragment>
             <EntryNavbar />
-            <div className={classes.card}>
-                <Heading heading="Sign in" />
-                <p className={classes.message}>
-                    Welcome back! Sign in to continue your journey.<br />Did you
+            <div className={classes.root}>
+                <div className={classes.card}>
+                    <Heading heading="Sign in" />
+                    <p className={classes.message}>
+                        Welcome back! Sign in to continue your journey.<br />Did you
                 <span className={classes.link}> forget your password?</span>
-                </p>
-                <FormControl>
-                    {error && <Typography className={classes.error}>{error}</Typography>}
-                    <TextField
-                        className={classes.textField}
-                        label="Email"
-                        type="email"
-                        inputProps={inputProp}
-                        value={values.email}
-                        onChange={handleChange("email")}
-                    />
-                </FormControl>
-                <FormControl className={classes.textField}>
-                    <form autoComplete="off">
-                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                        <Input
-                            id="standard-adornment-password"
+                    </p>
+                    <FormControl>
+                        {error && <Typography className={classes.error}>{error}</Typography>}
+                        <TextField
+                            className={classes.textField}
+                            label="Email"
+                            type="email"
                             inputProps={inputProp}
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            autoComplete="off"
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                    >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
+                            value={values.email}
+                            onChange={handleChange("email")}
                         />
-                    </form>
-                </FormControl>
-                <Button
-                    className={classes.continueButton}
-                    onClick={handleSubmit}
-                    size="large"
-                    variant="contained"
-                    disabled={loading}
-                >
-                    Login
+                    </FormControl>
+                    <FormControl className={classes.textField}>
+                        <form autoComplete="off">
+                            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                            <Input
+                                id="standard-adornment-password"
+                                inputProps={inputProp}
+                                type={values.showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                autoComplete="off"
+                                onChange={handleChange('password')}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </form>
+                    </FormControl>
+                    <Button
+                        className={classes.continueButton}
+                        onClick={handleSubmit}
+                        size="large"
+                        variant="contained"
+                        disabled={loading}
+                    >
+                        Login
                  </Button>
-                <p className={classes.message}>
-                    Don't have an account?
+                    <p className={classes.message}>
+                        Don't have an account?
                 <span className={classes.link}> Create a new account</span>
-                </p>
+                    </p>
+                </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
 
