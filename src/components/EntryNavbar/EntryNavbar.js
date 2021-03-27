5388cdcd7c7679c84efc2042da1d339cbb7bcd53
Ayush Acharya <ayushjash1323@gmail.com>
2021-03-27 18:32:25 +0530

    Added EntryNavbar functionalities

--- src/components/EntryNavbar/EntryNavbar.js
+++ src/components/EntryNavbar/EntryNavbar.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+import Logo from "../Logo/Logo";
+import MenuButton from "../MenuButton/MenuButton";
+
+const useStyles = makeStyles(theme => ({
+    header: {
+        background: "transparent",
+        backdropFilter: "blur(0.3rem)",
+        position: "sticky",
+        top: 0,
+        zIndex: 5,
+    },
+    nav: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        margin: "auto",
+        minHeight: "15vh",
+        [theme.breakpoints.down("sm")]: {
+            minHeight: "10vh",
+        },
+        width: "90%",
+    },
+}));
 
 function EntryNavbar() {
+    const classes = useStyles();
     return (
-        "navbar"
+        <header className={classes.header}>
+            <nav className={classes.nav}>
+                <Logo />
+                <MenuButton />
+            </nav>
+        </header>
     );
 }
 
