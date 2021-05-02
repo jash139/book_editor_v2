d4cbb975e74ed709d4e2fdd7fa6dcc8fb7a659bf
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-02 19:47:19 +0530

    Created WriteButton

---
+++ src/components/WriteButton/WriteButton.js
@@ -0,0 +1,12 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+function WriteButton() {
+    return (
+        <React.Fragment>
+            <Button variant="contained">New Book</Button>
+        </React.Fragment>
+    );
+}
+
+export default WriteButton;
