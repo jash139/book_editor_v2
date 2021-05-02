e12c328eb537f10a5faf064d4022a3de6094aa1f
Ayush Acharya <ayushjash1323@gmail.com>
2021-05-02 17:56:52 +0530

    Created ProfileLibrary and ProfileWork component

---
+++ src/components/ProfileLibrary/ProfileLibrary.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProfileLibrary() {
+    return (
+        "lib"
+    );
+}
+
+export default ProfileLibrary;
---
+++ src/components/ProfileWork/ProfileWork.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProfileWork() {
+    return (
+        "work"
+    );
+}
+
+export default ProfileWork;
